0c75f46465d77a5fe3b2e8602d6661d0e033b9ec
Nataliya R <n.rudenko@gmail.com>
2019-09-11 17:25:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function writeCards( [ "Ada", "Brendan", "Ali"], "birthday") {
   
   for (let i = 0; i < writeCards.length; i++ ) {
-    console.log(`Thank you, ${writeCards}, for the wonderful birthday gift!)
-  }
+    console.log(`Thank you, ${writeCards}, for the wonderful birthday gift!); }
 }
