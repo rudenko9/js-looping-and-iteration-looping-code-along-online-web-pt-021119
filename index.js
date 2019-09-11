cbacc43326990a3d43f2f182afb73a56a54c8220
Nataliya R <n.rudenko@gmail.com>
2019-09-11 18:20:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+  const message = []; 
+  
+  for (let i = 0; i < names.length; i++ ) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`); }
+    
+    return message; 
+}
+
+function  countdown(count) {
+
+  while(count >= 0) {
+    console.log(count);
+    count--; 
+  }
+
+}
